500569ed28dcad23c342c9b1fba05e956ab53e7d
烟汐忆梦_YM <140093497+Yanxiyimengya@users.noreply.github.com>
2024-02-06 00:02:51 +0800

    更新

--- 项目工程/演示项目/溺死光中DemoProject/Components/TextTyper/TextTyper.gd
+++ 项目工程/演示项目/溺死光中DemoProject/Components/TextTyper/TextTyper.gd
@@ -9,17 +9,46 @@ enum STATE {
 }; # 打字机状态
 
 var text : String = "";
-var char_node_list : Array[Control] = []; # 字符节点列表
-var char_node_pool : Array[Control] = []; # 字符节点池
+var typer_process : int = 0;					# 打字机文本进度
+var typer_speed : int = 4;						# 打字机速度
+var typer_process : int = 0;					# 打字机文本进度
+
+var char_node_list : Array[Control] = [];		# 字符节点列表
+var char_node_pool : Array[Control] = [];		# 字符节点池
 
 var typer_state : STATE = STATE.PROCESS;
 
-func new_char(char : String) :
-	var char_node : Label = Label.new(); # 构造字符节点
-	char_node.text = char[0]; # 获取到字符
-	self.add_child(char_node); # 添加入树
-	char_node_list.push_back(char_node); # 压入字符列表
-	pass; # 在末尾创建一个新的字符节点实例
+func new_char(char : String) -> Label:
+	var char_node : Label = null;
+	if (char_node_pool.is_empty()) :
+		char_node = Label.new();				# 构造字符节点
+		self.add_child(char_node);				# 添加入树
+		char_node_list.push_back(char_node);	# 压入字符列表
+	else :
+		char_node = char_node_pool.pop_back();	# 出栈
+		char_node.visible = true;
+	char_node.text = char[0];
+	return char_node; # 在末尾创建一个新的字符节点实例
+func delete_char() -> void :
+	var end_node = char_node_list.pop_back();	# 获取末尾节点
+	end_node.visible = false;
+	char_node_pool.push_back(end_node)
+	pass; # 在末尾移除一个字符节点实例
 
-func _process(delta) :
+func llex() -> Array[String] : 
+	var cmds : Array[String] = [];
+	return cmds; # 对接下来的一段文本进行词法拆分
+func cmd_llex() -> String :
+	var this_char : String = text[typer_process]; # 获取当前打字的字符
+	var cmd_text : String = "";
+	while (this_char != "}") :
+		cmd_text += this_char;
+		typer_process += 1;
+		this_char = text[typer_process]; # 更新字符
+	return cmd_text; # 获取接下来的一段文本命令参数(直到遇到 '}' ')
+
+
+func _process(delta : float) :
+	if (typer_state == STATE.PAUSED) :
+		return;
 	pass;
