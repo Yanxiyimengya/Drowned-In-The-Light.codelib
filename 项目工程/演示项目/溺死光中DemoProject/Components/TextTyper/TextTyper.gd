0579c757d19935a9943c0c99eb99ff5d45cf41f2
烟汐忆梦_YM <140093497+Yanxiyimengya@users.noreply.github.com>
2024-02-05 23:34:41 +0800

    Update TextTyper.gd

--- 项目工程/演示项目/溺死光中DemoProject/Components/TextTyper/TextTyper.gd
+++ 项目工程/演示项目/溺死光中DemoProject/Components/TextTyper/TextTyper.gd
@@ -3,12 +3,23 @@ class_name TextTyper;
 # 打字机组件
 
 enum STATE {
-	PAUSED,
-	PROCESS
-}
+	PAUSED,		# 停止
+	WAIT,		# 等待
+	PROCESS		# 运行
+}; # 打字机状态
 
 var text : String = "";
 var char_node_list : Array[Control] = []; # 字符节点列表
 var char_node_pool : Array[Control] = []; # 字符节点池
 
 var typer_state : STATE = STATE.PROCESS;
+
+func new_char(char : String) :
+	var char_node : Label = Label.new(); # 构造字符节点
+	char_node.text = char[0]; # 获取到字符
+	self.add_child(char_node); # 添加入树
+	char_node_list.push_back(char_node); # 压入字符列表
+	pass; # 在末尾创建一个新的字符节点实例
+
+func _process(delta) :
+	pass;
