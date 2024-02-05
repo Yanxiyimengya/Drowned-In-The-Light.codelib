fbbd8eb9aaa4ce3f2911f382138839ffafd5e277
烟汐忆梦_YM <140093497+Yanxiyimengya@users.noreply.github.com>
2024-02-06 00:55:56 +0800

    Update

--- 项目工程/演示项目/溺死光中DemoProject/Components/TextTyper/TextTyper.gd
+++ 项目工程/演示项目/溺死光中DemoProject/Components/TextTyper/TextTyper.gd
@@ -8,7 +8,7 @@ enum STATE {
 	PROCESS		# 运行
 }; # 打字机状态
 
-var text : String = "";
+@export var text : String = "";
 var typer_process : int = 0;					# 打字机文本进度
 var typer_speed : int = 4;						# 打字机速度
 
@@ -23,6 +23,7 @@ func new_char(char : String) -> Label:
 		char_node = Label.new();				# 构造字符节点
 		self.add_child(char_node);				# 添加入树
 		char_node_list.push_back(char_node);	# 压入字符列表
+		char_node_pool.push_back(char_node);	# 压入字符节点池
 	else :
 		char_node = char_node_pool.pop_back();	# 出栈
 		char_node.visible = true;
@@ -35,19 +36,26 @@ func delete_char() -> void :
 	pass; # 在末尾移除一个字符节点实例
 
 func llex() -> Array[String] : 
-	var cmds : Array[String] = [];
-	return cmds; # 对接下来的一段文本进行词法拆分
-func cmd_llex() -> String :
 	var this_char : String = text[typer_process]; # 获取当前打字的字符
+	if( this_char != "{") : return [];
+	var cmds : Array[String] = [];
 	var cmd_text : String = "";
-	while (this_char != "}") :
-		cmd_text += this_char;
+	while (true) :
 		typer_process += 1;
 		this_char = text[typer_process]; # 更新字符
-	return cmd_text; # 获取接下来的一段文本命令参数(直到遇到 '}' ')
+		if (this_char == "}" || (typer_process >= text.length())) :
+			cmds.push_back(cmd_text);
+			break;
+		elif (this_char == " ") :
+			cmds.push_back(cmd_text);
+			cmd_text = "";
+			continue;
+		cmd_text += this_char;
+	return cmds; # 对接下来的一段文本进行词法拆分
 
 
 func _process(delta : float) :
+	print(llex());
 	if (typer_state == STATE.PAUSED) :
 		return;
 	pass;
